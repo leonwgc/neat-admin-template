3d5707739e999033419926e0e0e2e2cc569a31ee
leonwgc <leon.wang@derbysoft.net>
2026-04-09 14:23:26 +0800

    refactor: remove unused AccountSwitcher component and related styles, hooks, and utility functions

--- src/components/AccountSwitcher/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @file components/AccountSwitcher/index.ts
- * @author leon.wang
- */
-export { AccountSwitcher, type Account } from './AccountSwitcher';
--- src/utils/helper.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs';
-
-export const renderTableDate = (date, dateFormat = 'YYYY-MM-DD HH:mm') => {
-  return date ? dayjs(date).format(dateFormat) : '--';
-};
-
-export const renderTableValue = (value) => {
-  return value || '--';
-};
