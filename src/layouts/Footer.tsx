dd6e52d8f1d3393e294e4674719db420f17dc7ec
leonwgc <leon.wang@derbysoft.net>
2026-04-09 15:40:29 +0800

    feat: update Footer component for improved rendering and layout adjustments

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -10,12 +10,24 @@ const Footer = ({ menuCollapsed }) => {
   return (
     <div className="app-footer">
       {menuCollapsed ? (
-        <div className="symbol" style={{ fontSize: 20 }}>
-          ©
-        </div>
+        <FadeIn duration={300} key={'0'}>
+          <div className="symbol" style={{ fontSize: 20 }}>
+            ©
+          </div>
+        </FadeIn>
       ) : (
-        <FadeIn duration={300}>
+        <FadeIn duration={300} key={'1'}>
           <div>© 2002 - 2026 德比软件 Inc.</div>
+          <div>
+            版权所有,
+            <a
+              target="_blank"
+              href="https://www.derbysoft.com/cn/privacy-policy/"
+              rel="noreferrer"
+            >
+              隐私政策
+            </a>
+          </div>
         </FadeIn>
       )}
     </div>
