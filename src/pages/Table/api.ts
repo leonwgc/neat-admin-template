a25115a07ba5a2cce3bda10cbf0b6ac53186ed06
leonwgc <leon.wang@derbysoft.net>
2026-04-09 13:53:48 +0800

    refactor: wrap getDataList response in a Promise for asynchronous handling

--- src/pages/Table/api.ts
+++ src/pages/Table/api.ts
@@ -1,8 +1,12 @@
 import { data } from './mock';
 
 export const getDataList = async ({ current, pageSize }) => {
-  return {
-    total: data.totals,
-    list: data.records.slice((current - 1) * pageSize, current * pageSize),
-  };
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        total: data.totals,
+        list: data.records.slice((current - 1) * pageSize, current * pageSize),
+      });
+    }, 500);
+  });
 };
