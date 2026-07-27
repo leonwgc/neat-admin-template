3050f64606d580953a40de20223beb62c026636e
leonwgc <leon.wang@derbysoft.net>
2026-07-27 18:00:11 +0800

    feat: add getSSOFrontendURI function for environment-specific SSO URL configuration

--- src/req/req.config.ts
+++ src/req/req.config.ts
@@ -1,1 +1,17 @@
 export const notVerifyLocation = ['/nav/no-permission', '/nav/404', '/nav/500'];
+
+// 获取 SSO 前端地址
+export const getSSOFrontendURI = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return 'http://localhost:5010';
+    case 'qa':
+      return 'https://sso.qa.derbysoft-test.com';
+    case 'uat':
+      return 'https://sso-uat.derbysoft-test.com';
+    case 'production':
+      return 'https://sso.derbysoftsec.com';
+    default:
+      return 'https://sso.derbysoftsec.com';
+  }
+};
