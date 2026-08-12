7831f2d3d13c1ca20232235994c13b3a3cd58d8b
leonwgc <leon.wang@derbysoft.net>
2026-08-12 17:41:36 +0800

    1

--- src/hooks/useReq.tsx
+++ src/hooks/useReq.tsx
@@ -3,7 +3,7 @@
  * @author leon.wang(leon.wang@derbysoft.net)
  */
 
-import { useCallback, type DependencyList } from 'react';
+import { useCallback } from 'react';
 import { useRequest } from 'ahooks';
 import { AxiosPromise } from 'axios';
 import { App } from '@derbysoft/neat-design';
@@ -12,38 +12,44 @@ import { onSuccessHandler } from '../helper';
 
 const t = (key) => i18n.t(key);
 
-type Options<TParams extends unknown[] = any[], TData = unknown> = {
-  manual?: boolean;
+type Options = Parameters<typeof useRequest>[1] & {
   async?: boolean; // default false
   toastDefaultError?: boolean; // toast operation failed when onError triggerd
-  onBefore?: (params: TParams) => void;
-  onSuccess?: (data: TData, params: TParams) => void;
-  // handle logical error
-  onFailed?: (data: TData, params: TParams, res?: TData) => void;
-  onFinally?: (params: TParams, data?: TData, e?: Error) => void;
-  defaultParams?: TParams;
-  refreshDeps?: DependencyList;
-  refreshDepsAction?: () => void;
-  loadingDelay?: number;
-  pollingInterval?: number;
-  pollingWhenHidden?: boolean;
-  pollingErrorRetryCount?: number;
-  refreshOnWindowFocus?: boolean;
-  focusTimespan?: number;
-  debounceWait?: number;
-  debounceLeading?: boolean;
-  debounceTrailing?: boolean;
-  debounceMaxWait?: number;
-  throttleWait?: number;
-  throttleLeading?: boolean;
-  throttleTrailing?: boolean;
-  cacheKey?: string;
-  cacheTime?: number;
-  staleTime?: number;
-  retryCount?: number;
-  retryInterval?: number;
-  ready?: boolean;
+  onFailed?: (data: unknown, params: unknown, res?: unknown) => void;
 };
+
+// type Options<TParams extends unknown[] = any[], TData = unknown> = {
+//   manual?: boolean;
+//   async?: boolean; // default false
+//   toastDefaultError?: boolean; // toast operation failed when onError triggerd
+//   onBefore?: (params: TParams) => void;
+//   onSuccess?: (data: TData, params: TParams) => void;
+//   // handle logical error
+//   onFailed?: (data: TData, params: TParams, res?: TData) => void;
+//   onFinally?: (params: TParams, data?: TData, e?: Error) => void;
+//   defaultParams?: TParams;
+//   refreshDeps?: DependencyList;
+//   refreshDepsAction?: () => void;
+//   loadingDelay?: number;
+//   pollingInterval?: number;
+//   pollingWhenHidden?: boolean;
+//   pollingErrorRetryCount?: number;
+//   refreshOnWindowFocus?: boolean;
+//   focusTimespan?: number;
+//   debounceWait?: number;
+//   debounceLeading?: boolean;
+//   debounceTrailing?: boolean;
+//   debounceMaxWait?: number;
+//   throttleWait?: number;
+//   throttleLeading?: boolean;
+//   throttleTrailing?: boolean;
+//   cacheKey?: string;
+//   cacheTime?: number;
+//   staleTime?: number;
+//   retryCount?: number;
+//   retryInterval?: number;
+//   ready?: boolean;
+// };
 
 export const errorHandler = (
   error,
@@ -65,11 +71,11 @@ export const errorHandler = (
 };
 
 const useReq = (
-  request: (...params: any[]) => AxiosPromise<ResponseDataType>,
-  options: Options = {},
+  request: (...params) => AxiosPromise<ResponseDataType>,
+  options?: Options,
 ) => {
   const { toast, notification } = App.useApp();
-  const { toastDefaultError } = options;
+  const { toastDefaultError } = options || {};
   const onError = useCallback(
     (error) => {
       errorHandler(error, notification, toastDefaultError, toast);
