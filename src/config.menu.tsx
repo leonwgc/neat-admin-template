c1d1b91b976f181a56e7aaf6cfc06317985c41be
leonwgc <leon.wang@derbysoft.net>
2026-08-14 10:42:02 +0800

    1

--- src/config.menu.tsx
+++ src/config.menu.tsx
@@ -106,114 +106,114 @@ export const menus: MenuItem[] = [
       },
     ],
   },
-  {
-    key: 'form',
-    get label() {
-      return t('menu.forms');
-    },
-    icon: <TableOutlined />,
-    permissions: [],
-    children: [
-      {
-        key: 'forms-table',
-        get label() {
-          return t('menu.table');
-        },
-        route: '/app/forms/table',
-        permissions: [],
-      },
-      {
-        key: 'form1',
-        get label() {
-          return t('menu.responsiveForm');
-        },
-        route: '/app/forms',
-        permissions: [operations.formRead],
-      },
-    ],
-  },
-  {
-    key: 'api-request',
-    get label() {
-      return t('menu.apiRequest');
-    },
-    icon: <APIOutlined />,
-    permissions: [],
-    children: [
-      {
-        key: 'api-demo',
-        get label() {
-          return t('menu.apiDemo');
-        },
-        route: '/app/api-request/api-demo',
-        permissions: [],
-      },
-    ],
-  },
-  {
-    key: 'result-page',
-    get label() {
-      return t('menu.resultPage');
-    },
-    icon: <WrenchOutlined />,
-    permissions: [],
-    children: [
-      {
-        key: 'no-permission',
-        get label() {
-          return t('menu.noPermission');
-        },
-        route: '/app/result-page/no-permission',
-      },
-      {
-        key: 'not-found',
-        get label() {
-          return t('menu.notFound');
-        },
-        route: '/app/result-page/not-found',
-      },
-      {
-        key: 'mantainance',
-        get label() {
-          return t('menu.mantainance');
-        },
-        route: '/app/result-page/mantainance',
-      },
-    ],
-  },
+  // {
+  //   key: 'form',
+  //   get label() {
+  //     return t('menu.forms');
+  //   },
+  //   icon: <TableOutlined />,
+  //   permissions: [],
+  //   children: [
+  //     {
+  //       key: 'forms-table',
+  //       get label() {
+  //         return t('menu.table');
+  //       },
+  //       route: '/app/forms/table',
+  //       permissions: [],
+  //     },
+  //     {
+  //       key: 'form1',
+  //       get label() {
+  //         return t('menu.responsiveForm');
+  //       },
+  //       route: '/app/forms',
+  //       permissions: [operations.formRead],
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'api-request',
+  //   get label() {
+  //     return t('menu.apiRequest');
+  //   },
+  //   icon: <APIOutlined />,
+  //   permissions: [],
+  //   children: [
+  //     {
+  //       key: 'api-demo',
+  //       get label() {
+  //         return t('menu.apiDemo');
+  //       },
+  //       route: '/app/api-request/api-demo',
+  //       permissions: [],
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'result-page',
+  //   get label() {
+  //     return t('menu.resultPage');
+  //   },
+  //   icon: <WrenchOutlined />,
+  //   permissions: [],
+  //   children: [
+  //     {
+  //       key: 'no-permission',
+  //       get label() {
+  //         return t('menu.noPermission');
+  //       },
+  //       route: '/app/result-page/no-permission',
+  //     },
+  //     {
+  //       key: 'not-found',
+  //       get label() {
+  //         return t('menu.notFound');
+  //       },
+  //       route: '/app/result-page/not-found',
+  //     },
+  //     {
+  //       key: 'mantainance',
+  //       get label() {
+  //         return t('menu.mantainance');
+  //       },
+  //       route: '/app/result-page/mantainance',
+  //     },
+  //   ],
+  // },
 
-  {
-    key: 'components',
-    get label() {
-      return t('menu.components');
-    },
-    icon: <WidgetOutlined />,
-    permissions: [],
-    children: [
-      {
-        key: 'image-upload',
-        get label() {
-          return t('menu.imageUpload');
-        },
-        route: '/app/components/image-upload',
-        permissions: [operations.imageUploadRead],
-      },
-      {
-        key: 'image-cropper',
-        get label() {
-          return t('menu.imageCropper');
-        },
-        route: '/app/components/image-cropper',
-        permissions: [operations.imageCropRead],
-      },
-      {
-        key: 'select-ime-demo',
-        get label() {
-          return t('menu.selectIMEDemo');
-        },
-        route: '/app/components/select-ime-demo',
-        permissions: [],
-      },
-    ],
-  },
+  // {
+  //   key: 'components',
+  //   get label() {
+  //     return t('menu.components');
+  //   },
+  //   icon: <WidgetOutlined />,
+  //   permissions: [],
+  //   children: [
+  //     {
+  //       key: 'image-upload',
+  //       get label() {
+  //         return t('menu.imageUpload');
+  //       },
+  //       route: '/app/components/image-upload',
+  //       permissions: [operations.imageUploadRead],
+  //     },
+  //     {
+  //       key: 'image-cropper',
+  //       get label() {
+  //         return t('menu.imageCropper');
+  //       },
+  //       route: '/app/components/image-cropper',
+  //       permissions: [operations.imageCropRead],
+  //     },
+  //     {
+  //       key: 'select-ime-demo',
+  //       get label() {
+  //         return t('menu.selectIMEDemo');
+  //       },
+  //       route: '/app/components/select-ime-demo',
+  //       permissions: [],
+  //     },
+  //   ],
+  // },
 ];
